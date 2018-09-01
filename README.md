b2df79b4637e1e5c4b2f69c38b3eb9bc70bf3c1b
Han <2han@live.com>
2018-09-01 15:43:59 -0700

    added screenshot

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Psychic-Game
+
+## Random Letter Guessing Game built with JavaScript
+
+### Link to Live Site
+[Psychic-Game](https://captnwalker.github.io/Psychic-Game2/)
+
+### Screenshot
+![Psychic-Game Screenshot](https://github.com/hanbanana/Psychic-Game/master/screenshot/Psychic-Game.jpg)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var loses = 0;
-var guessesLeft, guessedLetters, computerGuesses;
+var guessesLeft = "";
+var guessedLetters = "";
+var computerGuesses = "";
 
 resetGame();
 display();
